b88d2cb35ad818b5e7a62612df35d6f1448c43cc
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-05-19 12:47:57 -0400

    Exercise 2
    Updated index.js to include
    Express
    morgan
    fs
    error-handling middlewrae.

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express'),
+    morgan = require('morgan'),
+    fs = require('fs'),
+    path = require('path');
+const app = express(); 
+const bodyParser = require('bodyParser'),
+    methodOverride = require('method-override');
+    
+app.use(bodyParser.urlencoded({
+    extend: true
+}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+
+app.use(express.static('public'));
+app.use(morgan('common'));
+
+app.use(morgan('combined', {stream: accessLogStream}));
+
+app.get('/', (req, res) => {
+    res.send('Hello and Welcome to my API for movies');
+});
+
+app.get('/movies', (req, res) => {
+    let topMovies = [
+        { title: 'Star Wars: Episode IV - A New Hope',
+        director: 'George Lucas',
+        release: 'July 21, 1978'},
+    
+        { title: 'Seven',
+        director: 'David Fincher',
+        release: 'September 22, 1995'},
+    
+        {title: 'Star Wars: Episode V - The Empire Strikes Back',
+        director: 'Irvin Kershner',
+        release: 'May 21, 1980'},
+    
+        {title: 'Back to the Future',
+        director: 'Robert Zemeckis',
+        release: 'July 3, 1985'}, 
+        
+        {title: 'Inglorious Bastards',
+        director: 'Quentin Tarantino',
+        release: 'August 21, 2009'},  
+    
+        {title: 'Snatch',
+        director: 'Guy Ritchie',
+        release: 'January 19, 2001'},
+    
+        {title: 'Tombstone',
+        director: 'Geroge P. Cosmatos, Kevin Jarre',
+        release: 'December 25, 1993'},
+    
+        {title: 'Three Amigos',
+        director: 'John Landis',
+        release: 'December 12, 1986'},
+    
+        {title: 'Mean Girls',
+        director: 'Mark Waters',
+        release: 'April 20, 2004'},
+    
+        {title: 'The Boondock Saints',
+        director: 'Troy Duffy',
+        release: 'January 21, 2000'},
+    ];    
+    res.json(topMovies);
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('An Error Ocurred')
+})
+
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.')
+})
