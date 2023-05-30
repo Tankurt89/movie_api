4330d87854a27b948a8af228432f1232b7d32d9b
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-05-29 22:34:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ app.get('/users', passport.authenticate('jwt', {session: false}), (req, res) => 
     Users.find()
       .then((user) => {
         let maskUsers = [];
-        for (i=0; i<Users.length; i++) {maskedUsers[i] = MaskData.maskJSONFields(user[i], maskJSONoptions);}
+        for (let i=0; i<Users.length; i++) {maskedUsers[i] = MaskData.maskJSONFields(user[i], maskJSONOptions);}
         console.log('hi mom');
         console.log(maskUsers);
         res.status(201).json(maskUsers);
